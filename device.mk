04831cc406e9fec72bdfafc47fd3d9059d82f6f5
Tim Zimmermann <tim@linux4.de>
2021-11-21 20:17:51 +0100

    veux: Uprev audio HAL to V7
    Updated audio policy by running:
    
    'hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/xiaomi/veux/audio/audio_policy_configuration.xml 7.0'
    
    with some manual intervention.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -45,8 +45,8 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl
@@ -85,7 +85,7 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/holi/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/$(AUDIO_SKU_DIR)/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/$(AUDIO_SKU_DIR)/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/$(AUDIO_SKU_DIR)/r_submix_audio_policy_configuration.xml
 
 $(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
