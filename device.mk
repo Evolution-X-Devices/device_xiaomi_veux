23be56c83101c1054928dec529d3f4d862e9c460
Lunarixus <Nathan@lunarixus.dev>
2022-01-30 11:20:20 +0530

    veux: Remove unwanted GApps
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- device.mk
+++ device.mk
@@ -513,3 +513,7 @@ PRODUCT_PACKAGES += \
 
 # Inherit from proprietary targets
 $(call inherit-product, vendor/xiaomi/veux/veux-vendor.mk)
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
