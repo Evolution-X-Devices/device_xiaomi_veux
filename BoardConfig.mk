6a2fbf8aab6e7e842c43b58199fe92304c5592a7
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    veux: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 DEVICE_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+    hardware/qcom-caf/common/compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += \
     sn100
