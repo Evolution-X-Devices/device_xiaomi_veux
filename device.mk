0bc849470e28b41c21efd8bcf088f71cc441e6a9
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    veux: Build AOSP Radio HIDL->AIDL Wrapper
    Depends-On: I790728a9be289042e5a07589934a2dd50463a507
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- device.mk
+++ device.mk
@@ -456,6 +456,7 @@ PRODUCT_PACKAGES += \
     librmnetctl
 
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
