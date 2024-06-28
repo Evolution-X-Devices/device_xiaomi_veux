45ce21b06e0129755c3f3be1e8026678fbc4dcbf
Prathamk07 <prathamkalra.dev@gmail.com>
2024-06-28 09:55:27 +0530

    veux: Added Interface for Camera
    Added an Interface for Camera that was required after QPR3.
    Fixes Broken Camera in veux after QPR3.
    Imports missing libraries for camera.
    
    Signed-off-by: Prathamk07 <prathamkalra.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -105,13 +105,23 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libpiex_shim
+    libcamera2ndk_vendor \
+    libstdc++_vendor \
+    libpiex_shim \
+    liblz4.vendor \
+    libutilscallstack.vendor \
+    libcamera2ndk_vendor \
+    libion.vendor \
+    libgui_vendor \
+    libpng.vendor 
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
