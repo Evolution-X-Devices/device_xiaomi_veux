4cf933e2280fa392dcd1463342e93baaa5e0ed45
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-09-14 19:03:59 +0545

    veux: Setup prebuilt audio HAL
    Drop BOARD_USES_ALSA_AUDIO as it will declare audio.primary.holi, which
    is shipped prebuilt.
    Declare TARGET_PROVIDES_AUDIO_EXTNS as we ship prebuilt liba2dpoffload.
    
    Drop more flags which aren't required.
    
    Change-Id: I9e52f6b2bc6e105597e4f1ee291674f7af43fba3
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -54,23 +54,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.primary.default \
-    audio.primary.holi \
     audio.r_submix.default \
     audio.usbv2.default
 
 PRODUCT_PACKAGES += \
     audioadsprpcd \
-    liba2dpoffload \
-    libbatterylistener \
-    libcomprcapture \
-    libexthwplugin \
-    libhdmiedid \
-    libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libsndmonitor \
-    libspkrprot \
     libvolumelistener \
     sound_trigger.primary.holi:32
 
