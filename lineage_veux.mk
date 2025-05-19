fb2c4bf717f6c6f4ce73e53276b832b7727ab3e2
Onelots <onelots@onelots.fr>
2025-05-20 01:39:37 +0200

    veux: enable viperfx
    Signed-off-by: Onelots <onelots@onelots.fr>

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -14,6 +14,8 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_INCLUDE_VIPERFX := true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
