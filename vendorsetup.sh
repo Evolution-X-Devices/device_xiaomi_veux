54c034e4b985b1b4fa21974633c42c88fd8216f0
Onelots <onelots@onelots.fr>
2025-05-20 01:06:24 +0200

    veux: drop miuicamera
    * This memecam has video recording broken for a while, and AGC is way better...
    
    Signed-off-by: Onelots <onelots@onelots.fr>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,10 +17,6 @@ git clone --depth=1 https://github.com/PaimonLab/kernel_xiaomi_veux.git kernel/x
 echo "cloning AGC camera..."
 git clone --depth=1 https://gitlab.com/takixamru/vendor_xiaomi_agc-camera.git vendor/xiaomi/AGC-Camera 
 
-# MIUI Camera (Lecia)
-echo "Cloning MIUI Camera (Lecia)..."
-git clone https://github.com/Onelots-Devices-Playground/vendor_xiaomi_miuicamera-veux vendor/xiaomi/miuicamera-veux
-
 echo "Cloning Sony Dolby"
 git clone https://github.com/userariii/vendor_sony_dolby.git vendor/sony/dolby
 
