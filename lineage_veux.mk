15e475ab29aefbd8178a0602f22f69278964c5ee
TechPanelGM <giomook2003@gmail.com>
2025-09-07 14:47:02 +0200

    veux: Include MiuiCamera

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -36,3 +36,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="veux_global-user 13 TKQ1.221114.001 V816.0.12.0.TKCMIXM release-keys" \
     BuildFingerprint=Redmi/veux_global/veux:13/TKQ1.221114.001/V816.0.12.0.TKCMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_DEVICE)
+
+# Camera
+include device/xiaomi/camera/miuicamera.mk
